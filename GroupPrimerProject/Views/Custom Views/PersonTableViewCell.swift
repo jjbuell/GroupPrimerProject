e4981f7b4cfe051c8991c8af52d9da38430b43fe
Bryson Jones <bmjones801@gmail.com>
2021-06-14 11:09:36 -0600

    Model, ModelController, StoryBoard, CustomCell, TableViewController

---
+++ GroupPrimerProject/Views/Custom Views/PersonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonTableViewCell.swift
+//  GroupPrimerProject
+//
+//  Created by Bryson Jones on 6/14/21.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var personNameLabel: UILabel!
+
+}
