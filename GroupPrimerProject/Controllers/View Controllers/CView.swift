3d28b60e5315e64ec113fa1be2d1915049fda070
James Phillips <jamesphillips2@icloud.com>
2021-06-14 07:15:14 -1000

    add DetailVC

---
+++ GroupPrimerProject/Controllers/View Controllers/CView.swift
@@ -0,0 +1,20 @@
+//
+//  CView.swift
+//  GroupPrimerProject
+//
+//  Created by James Phillips on 6/14/21.
+//
+
+import SwiftUI
+
+struct CView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CView_Previews: PreviewProvider {
+    static var previews: some View {
+        CView()
+    }
+}
